841d0aaec0d25d0b0c379e85c996780cf5543131
yaonb <Yaonbiz@yandex.ru>
2024-06-12 02:07:05 +0300

    1.1. Создание объекта [#165452]

---
+++ src/main/java/oop/Student.java
@@ -0,0 +1,27 @@
+package oop;
+
+public class Student {
+    public void music() {
+        System.out.println("Tra tra tra");
+    }
+
+    public void drumm() {
+        System.out.println("Boom Boom Boom");
+    }
+
+    public void sing() {
+        System.out.println("I believe I can fly");
+    }
+
+    public static void main(String[] args) {
+        Student vasya = new Student();
+        Student petya = new Student();
+        vasya.drumm();
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.sing();
+        petya.sing();
+        petya.sing();
+    }
+}
