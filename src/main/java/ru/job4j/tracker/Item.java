8abe998b65d11eafaa0acc453a87052c46e46b71
Genad <yaonbiz@yandex.ru>
2024-08-22 01:32:46 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,19 +1,36 @@
 package ru.job4j.tracker;
+import java.time.LocalDateTime;
 
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created;
 
     public Item() {
+        this.created = LocalDateTime.now();
+    }
+
+    public Item(LocalDateTime created) {
+            this.created = created;
     }
 
     public Item(String name) {
         this.name = name;
+        this.created = LocalDateTime.now();
     }
 
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+        this.created = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
     }
 
     public int getId() {
