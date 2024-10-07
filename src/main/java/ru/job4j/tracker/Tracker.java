28c328e3a3c04734b6492267ecf348fe8251e395
Genad <yaonbiz@yandex.ru>
2024-10-08 00:21:07 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,26 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        if (items[0] != null) {
+            return Arrays.copyOf(items, size);
+        }
+        return items;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[items.length];
+        int count = 0;
+
+        for (Item item : items) {
+            if (item != null && item.getName().equals(key)) {
+                result[count] = item;
+                count++;
+            }
+        }
+        return Arrays.copyOf(result, count);
     }
 
     public Item findById(int id) {
