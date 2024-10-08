9c406198216d16d20abd039fef74d8ae998f7d6d
Genad <yaonbiz@yandex.ru>
2024-10-08 14:05:49 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        for (int i = 0; i < value.length; i++) {
+            try {
+               if (value[i] != null && value[i].equals(key)) {
+                   return i;
+                }
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
+        throw new ElementNotFoundException("Element not found: " + key);
+    }
+
+    public static void main(String[] args) {
+        String[] client = {"Алекс", "Мария", "Дмитрий", "Анна", "Сергей", "Елена", "Игорь", "Наталья", "Михаил", "Ольга"};
+        String keyClient = "Дмитрий";
+
+        try {
+            int index = FindEl.indexOf(client, keyClient);
+            System.out.println("Ключивой клиент номер: " + index);
+        } catch (ElementNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
