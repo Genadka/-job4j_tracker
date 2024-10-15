bd36690c8b45b2b73d789df6f6bc7a1dcc549370
Genad <yaonbiz@yandex.ru>
2024-10-16 00:13:02 +0300

    0.3. Тестирование исключений с junit [#219366 #516409]

--- src/test/java/ru/job4j/ex/FactorialTest.java
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.ex;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -15,5 +16,12 @@ class FactorialTest {
                     factorial.calc(-1);
                 });
         assertThat(exception.getMessage()).isEqualTo("Number could not be less than 0");
+    }
+
+    @Test
+    public void whenCorrect() {
+        Factorial factorial = new Factorial();
+       int result = factorial.calc(6);
+       assertEquals(720, result);
     }
 }
