767d7519f680554855167c28b3db192207ee1b90
Genad <yaonbiz@yandex.ru>
2025-01-08 20:03:41 +0300

    1. Интерфейс Queue [#504942]

---
+++ src/main/java/ru/job4j/queue/Customer.java
@@ -0,0 +1,14 @@
+package ru.job4j.queue;
+
+public record Customer(String name, int amount) {
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public int amount() {
+        return amount;
+    }
+}
