7d39ab82e8bbb3e8bab7741e66cef220c9df4724
Genad <yaonbiz@yandex.ru>
2025-01-10 00:06:16 +0300

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PassportOfficeTest {
+    @Test
+    public void whenTestAddMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+
+    @Test
+    public void isFalse() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Petr");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        boolean result = office.add(citizen2);
+        assertThat(result).isFalse();
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+}
