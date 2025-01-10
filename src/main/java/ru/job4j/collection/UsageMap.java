a30335e5f9feb051be178c884df00eca9f79436c
Genad <yaonbiz@yandex.ru>
2025-01-10 09:54:42 +0300

    0. Map, HashMap [#212652 #527873]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("post@post", "Почтовв Почтальон");
+        hashMap.put("post@post", "Почтовв Почтов");
+        hashMap.put("post@post1", "Почтовв Почтальон1");
         for (String key: hashMap.keySet()) {
             String value = hashMap.get(key);
             System.out.println(key + " = " + value);
